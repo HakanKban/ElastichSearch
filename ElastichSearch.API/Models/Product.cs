cc5442310d2d20db687f9389435435be90ce3618
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2023-09-11 20:02:29 +0300

    repository sınıfı oluşturuldu.

--- ElastichSearch.API/Models/Product.cs
+++ ElastichSearch.API/Models/Product.cs
@@ -1,11 +1,14 @@
-﻿namespace ElastichSearch.API.Models;
+﻿using Nest;
+
+namespace ElastichSearch.API.Models;
 public class Product
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
+    [PropertyName("_id")]
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
     public decimal Price { get; set; }
     public int Stock { get; set; }
     public DateTime Created { get; set; }
-    public DateTime Updated { get; set; }
-    public ProductFeature Feature { get; set; }
+    public DateTime? Updated { get; set; }
+    public ProductFeature? Feature { get; set; }
 }
---
+++ ElastichSearch.API/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using ElastichSearch.API.Models;
+using Nest;
+
+namespace ElastichSearch.API.Repository
+{
+    public class ProductRepository
+    {
+        private readonly ElasticClient _client;
+        public ProductRepository(ElasticClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task<Product?> SaveAsync(Product product)
+        {
+            product.Created = DateTime.Now;
+
+            var response = await _client.IndexAsync(product, x => x.Index("products"));//Index kaydeder. Tablo ismi products olur.
+            if(!response.IsValid) return null;
+
+            product.Id = response.Id;
+
+            return product;
+        }
+    }
+}
