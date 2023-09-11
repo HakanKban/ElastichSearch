5c736c3852b013375469ddbd27f05013366eb30d
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2023-09-11 19:51:20 +0300

    model tanımlandı.

---
+++ ElastichSearch.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ElastichSearch.API.Models;
+public class Product
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public ProductFeature Feature { get; set; }
+}
